0d50a05065751f8573fff3eb6f00a1095d3394e6
Paul McLanahan <pmac@mozilla.com>
2017-05-31 16:29:54 -0400

    Add a diffing tool and update configs

---
+++ bin/config-diff.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import sys
+from subprocess import check_output
+
+from envcat import get_unique_vars, parse_env_file
+
+
+def get_local_config(app_name):
+    return get_unique_vars([
+        'configs/global.env',
+        'configs/usw.env',
+        'configs/{}.env'.format(app_name),
+    ])
+
+
+def get_current_config(app_name):
+    output = check_output('DEIS_PROFILE=usw deis config:list --oneline -a {}'.format(app_name), shell=True)
+    return parse_env_file(output.decode('utf-8').split())
+
+
+def main(app_name):
+    local_vars = get_local_config(app_name)
+    remote_vars = get_current_config(app_name)
+    diff = False
+    for key, value in local_vars.items():
+        if key not in remote_vars or value != remote_vars[key]:
+            diff = True
+            print('{var_name}:\n  current value: {old_val}\n      new value: {new_val}\n'.format(
+                var_name=key,
+                old_val=remote_vars.get(key) or '(none)',
+                new_val=value))
+    if not diff:
+        print('configs are identical')
+
+if __name__ == '__main__':
+    main(sys.argv[1])
