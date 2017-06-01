134e270758cc8d389bfe9aae6f7b5b33ceb9df55
Paul McLanahan <pmac@mozilla.com>
2017-06-01 01:38:11 -0400

    Add region support to diff tool

--- bin/config-diff.py
+++ bin/config-diff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import sys
 from subprocess import check_output
 
@@ -22,26 +23,42 @@ def get_local_config(app_name):
     ])
 
 
-def get_current_config(app_name):
-    output = check_output('DEIS_PROFILE=usw deis config:list --oneline -a {}'.format(app_name), shell=True)
+def get_current_config(app_name, region):
+    deis_bin = CLUSTERS[region]
+    output = check_output('DEIS_PROFILE={} {} config:list --oneline -a {}'.format(region, deis_bin, app_name),
+                          shell=True)
     return parse_env_file(output.decode('utf-8').split())
 
 
-def main(app_name):
+def get_diff(app_name, region):
     local_vars = get_local_config(app_name)
-    remote_vars = get_current_config(app_name)
-    diff = False
+    remote_vars = get_current_config(app_name, region)
+    diff = []
     for key, value in local_vars.items():
         if key not in remote_vars or value != remote_vars[key]:
-            diff = True
-            print('{var_name}:\n  current value: {old_val}\n      new value: {new_val}\n'.format(
-                var_name=key,
-                old_val=remote_vars.get(key) or '(none)',
-                new_val=value))
-    if diff:
-        return 0
-    else:
+            diff.append((key, remote_vars.get(key) or '(none)', value))
+
+    return diff
+
+
+def main(app_name):
+    parser = argparse.ArgumentParser(description='Show differences between repo and app configs')
+    parser.add_argument('app', metavar='APP_NAME', help='Deis app name')
+    parser.add_argument('-r', '--region', default='usw', choices=CLUSTERS.keys(),
+                        help='Deis cluster with which to diff (default: usw)')
+    args = parser.parse_args()
+    diff = get_diff(args.app, args.region)
+    if not diff:
         return 'configs are identical'
+
+    for key, old_val, new_val in diff:
+        print('{var_name}:\n  current value: {old_val}\n      new value: {new_val}\n'.format(
+            var_name=key,
+            old_val=old_val,
+            new_val=new_val))
+
+    return 0
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1]))
