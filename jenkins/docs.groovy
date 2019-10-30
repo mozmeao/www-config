9bc9709007fc4d0ff684b5bb1d70a170ab3481ee
Paul McLanahan <pmac@mozilla.com>
2019-10-30 16:59:51 -0400

    Remove all old docs build and deis config machinery

--- jenkins/docs.groovy
+++
@@ -1,6 +0,0 @@
-sh 'bin/docker-docs.sh'
-withCredentials([[$class: 'StringBinding',
-                  credentialsId: 'GH_TOKEN_MOZMAR_ROBOT',
-                  variable: 'GH_TOKEN']]) {
-    sh 'bin/publish-docs.sh'
-}
