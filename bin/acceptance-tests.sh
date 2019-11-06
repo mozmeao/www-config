991ee67e3933e4c08f4cb7f4a305114064bd112d
Josh Mize <jgmize@gmail.com>
2019-11-06 16:34:06 -0600

    Add .gitlab-ci.yml dev only
    Add iowa-a/bedrock-dev k8s yaml from git-sync-operator branch: e4ca0d8

---
+++ bin/acceptance-tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+case $1 in
+    chrome)
+        BROWSER_NAME=chrome
+        PLATFORM="Windows 10"
+        ;;
+    firefox)
+        BROWSER_NAME=firefox
+        BROWSER_VERSION="57.0"
+        PLATFORM="Windows 10"
+        ;;
+    ie)
+        BROWSER_NAME="internet explorer"
+        PLATFORM="Windows 10"
+        ;;
+    ie9)
+        BROWSER_NAME="internet explorer"
+        BROWSER_VERSION="9.0"
+        PLATFORM="Windows 7"
+        MARK_EXPRESSION=sanity
+        ;;
+    download)
+        DRIVER=
+        MARK_EXPRESSION=download
+        ;;
+    headless|*)
+        DRIVER=
+        MARK_EXPRESSION=headless
+        ;;
+esac
+
+docker run --rm \
+    -e "DRIVER=${DRIVER}" \
+    -e "SAUCELABS_USERNAME=${SAUCELABS_USERNAME}" \
+    -e "SAUCELABS_API_KEY=${SAUCELABS_API_KEY}" \
+    -e "BROWSER_NAME=${BROWSER_NAME}" \
+    -e "BROWSER_VERSION=${BROWSER_VERSION}" \
+    -e "PLATFORM=${PLATFORM}" \
+    -e "MARK_EXPRESSION=${MARK_EXPRESSION}" \
+    -e "BASE_URL=${BASE_URL}" \
+    mozorg/bedrock_test bin/run-integration-tests.sh
---
+++ bin/rollout-status.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for DEPLOY in ${1}/*deploy.yaml; do
+    kubectl rollout status -f ${DEPLOY}
+done
