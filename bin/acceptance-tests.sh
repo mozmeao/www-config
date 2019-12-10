aa863713c568445d6cd55e692348aff426d99e48
Josh Mize <jgmize@gmail.com>
2019-12-10 11:33:36 -0600

    docker pull $TEST_IMAGE before docker run

--- bin/acceptance-tests.sh
+++ bin/acceptance-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-docker pull mozorg/bedrock_test
+docker pull ${TEST_IMAGE:=mozorg/bedrock_test}
 docker run --rm \
     -e "DRIVER=${DRIVER}" \
     -e "SAUCELABS_USERNAME=${SAUCELABS_USERNAME}" \
@@ -11,4 +11,4 @@ docker run --rm \
     -e "MARK_EXPRESSION=${MARK_EXPRESSION}" \
     -e "BASE_URL=${BASE_URL}" \
     -e "PYTEST_PROCESSES=${PYTEST_PROCESSES:=4}" \
-    ${TEST_IMAGE:=mozorg/bedrock_test} bin/run-integration-tests.sh
+    ${TEST_IMAGE} bin/run-integration-tests.sh
