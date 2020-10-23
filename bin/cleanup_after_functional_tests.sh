c8933d88b5835270abaa6e71ff3f339100975140
Giorgos Logiotatidis <seadog@sealabs.net>
2020-10-23 12:34:23 +0300

    Cleanup only after tests run.

--- bin/cleanup_after_functional_tests.sh
+++ bin/cleanup_after_functional_tests.sh
@@ -7,6 +7,12 @@ then
     exit 0;
 fi
 
+# Copy artifacts from container to host to make them available for upload to GitLab
+docker cp "bedrock-${CI_JOB_ID}:/app/tests/results" "results-${CI_JOB_ID}"
+
+# Now that we copied artifact, remove container.
+docker rm "bedrock-${CI_JOB_ID}"
+
 if [ "${DRIVER}" = "Remote" ]; then
     docker-compose \
         -p "selenium-hub-${CI_JOB_ID}" \
