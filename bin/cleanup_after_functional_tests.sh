aec23f0cb6e0aeb632e7286fb87c7bcf0a2cfea4
Giorgos Logiotatidis <seadog@sealabs.net>
2020-10-23 12:32:53 +0300

    Don't cleanup if no tests run.

--- bin/cleanup_after_functional_tests.sh
+++ bin/cleanup_after_functional_tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
+if [ -z "${BASE_URL:-}" ];
+then
+    # No BASE_URL set, thus no tests run. Nothing to cleanup
+    exit 0;
+fi
+
 if [ "${DRIVER}" = "Remote" ]; then
     docker-compose \
         -p "selenium-hub-${CI_JOB_ID}" \
