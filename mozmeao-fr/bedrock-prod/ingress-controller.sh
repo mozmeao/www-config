91892a9759ecd0a9e0b03365edaf64c42c5c7a60
floatingatoll <r@floatingatoll.nu>
2021-08-30 09:23:08 -0700

    bedrock: Pin ingress-nginx to v0.* to defer Kubernetes upgrade

--- mozmeao-fr/bedrock-prod/ingress-controller.sh
+++ mozmeao-fr/bedrock-prod/ingress-controller.sh
@@ -3,8 +3,12 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
+# We pin to version ^0 here to exclude ingress-nginx 1.*,
+# which require a Kubernetes upgrade. 20210830 atoll
+
 HELMOPTIONS=$(cat << EOM
   --namespace $NS \
+  --version ^0 \
   -f $DEPLOYMENT/helm_configs/ingress.yml \
   bedrock-prod-ingress ingress-nginx/ingress-nginx
 EOM
