0ee87c9b0289005714bfec76930d04938a745b0a
atoll <atoll@mozilla.com>
2021-08-30 09:49:12 -0700

    bedrock-fr: Pin ingress-nginx *chart* to 3.36.0 to defer Kubernetes upgrade

--- mozmeao-fr/bedrock-prod/ingress-controller.sh
+++ mozmeao-fr/bedrock-prod/ingress-controller.sh
@@ -3,12 +3,13 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-# We pin to version ^0 here to exclude ingress-nginx 1.*,
-# which require a Kubernetes upgrade. 20210830 atoll
+# We pin to helm chart version 3.36.0, which provides appVersion 0.49.0,
+# which is the latest release as of today that doesn't contain app 1.0+
+# with a breaking change to require kubeVersion 1.19+. -- 20210830 atoll
 
 HELMOPTIONS=$(cat << EOM
   --namespace $NS \
-  --version ^0 \
+  --version 3.36.0 \
   -f $DEPLOYMENT/helm_configs/ingress.yml \
   bedrock-prod-ingress ingress-nginx/ingress-nginx
 EOM
